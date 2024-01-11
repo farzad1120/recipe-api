54b976a1967b805339cafd01efc5d756c794dd80
Farzad M. Zarasvand <iam@farzad.eu>
2024-01-11 23:19:22 +0100

    write first two tests

---
+++ app/app/calc.py
@@ -0,0 +1,23 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """
+    Adds two numbers
+    :param x: first number
+    :param y: second number
+    :return: result of addition
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Subtracts two numbers
+    :param x: first number
+    :param y: second number
+    :return: result of subtraction
+    """
+    return x - y
